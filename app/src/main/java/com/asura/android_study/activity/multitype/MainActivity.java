c614108c8cfecdcaea38fbd7730c218955282897
Asura <709238339@qq.com>
2018-06-27 15:42:03 +0800

    修改kotlin版本

---
+++ app/src/main/java/com/asura/android_study/activity/multitype/MainActivity.java
@@ -0,0 +1,15 @@
+package com.asura.android_study.activity.multitype;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.asura.android_study.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
