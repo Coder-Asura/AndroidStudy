646eccd346a9364a65aa0eb9d8adb168c8baa0f4
Asura <709238339@qq.com>
2018-06-30 10:34:45 +0800

    获取手机图片TODO

---
+++ app/src/main/java/com/asura/android_study/activity/album/AlbumActivity.java
@@ -0,0 +1,94 @@
+package com.asura.android_study.activity.album;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import com.asura.android_study.R;
+import com.asura.android_study.activity.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author Created by Asura on 2018/6/28 17:17.
+ */
+public class AlbumActivity extends BaseActivity {
+    @Override
+    public int setLayoutId() {
+        return R.layout.activity_album;
+    }
+
+    @Override
+    public void initView() {
+
+        new AsyncTask<Void, Integer, ArrayList<HashMap<String, String>>>() {
+
+            @Override
+            protected ArrayList<HashMap<String, String>> doInBackground(Void... voids) {
+                return getAllPictures(AlbumActivity.this);
+            }
+
+            @Override
+            protected void onPostExecute(ArrayList<HashMap<String, String>> hashMaps) {
+                super.onPostExecute(hashMaps);
+                Log.d("asura", "onPostExecute" + hashMaps.size());
+            }
+        }.execute();
+    }
+
+    /**
+     * 得到本地图片文件
+     *
+     * @param context
+     * @return
+     */
+    public static ArrayList<HashMap<String, String>> getAllPictures(Context context) {
+        ArrayList<HashMap<String, String>> picturemaps = new ArrayList<>();
+        HashMap<String, String> picturemap;
+        ContentResolver cr = context.getContentResolver();
+        //先得到缩略图的URL和对应的图片id
+        Cursor cursor = cr.query(
+                MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+                new String[]{
+                        MediaStore.Images.Thumbnails.IMAGE_ID,
+                        MediaStore.Images.Thumbnails.DATA
+                },
+                null,
+                null,
+                null);
+        if (cursor.moveToFirst()) {
+            do {
+                picturemap = new HashMap<>();
+                picturemap.put("image_id_path", cursor.getInt(0) + "");
+                picturemap.put("thumbnail_path", cursor.getString(1));
+                picturemaps.add(picturemap);
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
+        //再得到正常图片的path
+        for (int i = 0; i < picturemaps.size(); i++) {
+            picturemap = picturemaps.get(i);
+            String media_id = picturemap.get("image_id_path");
+            cursor = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    new String[]{
+                            MediaStore.Images.Media.DATA
+                    },
+                    MediaStore.Audio.Media._ID + "=" + media_id,
+                    null,
+                    null
+            );
+            if (cursor.moveToFirst()) {
+                do {
+                    picturemap.put("image_id", cursor.getString(0));
+                    picturemaps.set(i, picturemap);
+                } while (cursor.moveToNext());
+                cursor.close();
+            }
+        }
+        return picturemaps;
+    }
+}
