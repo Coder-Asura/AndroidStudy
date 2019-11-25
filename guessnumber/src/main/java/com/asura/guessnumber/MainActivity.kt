de7bce9517d4c364b77319c156167b8d94f172e7
Asura <qq709238339@users.noreply.github.com>
2019-11-25 21:12:34 +0800

    add guess number game project

---
+++ guessnumber/src/main/java/com/asura/guessnumber/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.asura.guessnumber
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var input: Int = -1
+    var answer: Int = -1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_ask.setOnClickListener {
+            try {
+                input = input_ask.text.toString().toInt()
+            } catch (e: Exception) {
+                Toast.makeText(this, "请输入0-100的数字！！！", Toast.LENGTH_SHORT).show()
+            }
+            if (input < 0 || input > 100) {
+                Toast.makeText(this, "请输入0-100的数字！！！", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            Toast.makeText(this, "请答题人答题！", Toast.LENGTH_SHORT).show()
+            input_ask.setText("")
+            tv_result.text = ""
+        }
+
+        btn_answer.setOnClickListener {
+            try {
+                answer = input_answer.text.toString().toInt()
+            } catch (e: Exception) {
+                Toast.makeText(this, "请输入0-100的数字！！！", Toast.LENGTH_SHORT).show()
+            }
+            if (answer < 0 || answer > 100) {
+                Toast.makeText(this, "请输入0-100的数字！！！", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (input == answer) {
+                AlertDialog.Builder(this)
+                        .setTitle("666")
+                        .setMessage("你他娘的真是个天才！！！")
+                        .setNegativeButton("再玩一次") { dialog, _ ->
+                            input_ask.setText("")
+                            input_answer.setText("")
+                            tv_result.text = ""
+                            dialog.dismiss()
+                        }.create().show()
+
+                tv_result.text = "你他娘的真是个天才！！！"
+            } else if (input < answer) {
+                tv_result.text = "再小一点！"
+                Toast.makeText(this, "再小一点！", Toast.LENGTH_SHORT).show()
+            } else {
+                tv_result.text = "再大一点！"
+                Toast.makeText(this, "再大一点！", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
