6e29e0c4471e74d2727b6d3a963966bfd9903f57
Asura <qq709238339@users.noreply.github.com>
2019-11-25 23:56:48 +0800

    update to support androidx

--- mvp-framework/src/main/java/com/asura/mvp_framework/support/view/BaseMvpFragment.java
+++ mvp-framework/src/main/java/com/asura/mvp_framework/support/view/BaseMvpFragment.java
@@ -2,9 +2,11 @@ package com.asura.mvp_framework.support.view;
 
 import android.content.Context;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.Fragment;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
