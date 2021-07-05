5f05d6baa41d3da8a0ddc998adff3b671618e81b
Asura <709238339@qq.com>
2021-07-05 22:16:30 +0800

    upgrade sdk to 30

--- app/src/main/java/com/asura/android_study/application/MyApplication.kt
+++ app/src/main/java/com/asura/android_study/application/MyApplication.kt
@@ -1,6 +1,8 @@
 package com.asura.android_study.application
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 
 import com.asura.a_log.ALog
 import com.asura.android_study.BuildConfig
@@ -12,5 +14,10 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ALog.initLog(BuildConfig.LOG_ENABLE, "Asura")
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
