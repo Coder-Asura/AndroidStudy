43b7b8471770ef89eccb7edbab4ad8e1b3510914
asuraliu <asuraliu@vesync.com.cn>
2021-11-02 15:56:51 +0800

    所有行都准确

--- app/src/main/java/com/asura/android_study/utils/LightSectionsUtils.kt
+++ app/src/main/java/com/asura/android_study/utils/LightSectionsUtils.kt
@@ -18,30 +18,31 @@ object LightSectionsUtils {
     //    }
 
     private const val MIN_SECTIONS_COUNT = 2
-    private const val MAX_SECTIONS_COUNT = 150
+    private const val MAX_SECTIONS_COUNT = 15
     private const val SPAN_COUNT = 5
 
 
     /**
      * 获取灯带段列表
+     * step1: 计算真实行数及最后一行个数
+     * step2: 奇数行位置一致，偶数行位置反序,空白位置补全占位空格
      * @param max 最大几段
      * @param spanCount 每行几段
      * @param count 要几段
      *
      * //0  , 1  , 2  , 3  , 4 ,
-     * //8  , 7  , 6  , 5  , X ,
-     * //X  , 9  , 10 , 11 , 12,
-     * //X  , X  , 14 , 13 , X ,
+     * //8  , 7  , 6  , 5  , 4 ,
+     * //8  , 9  , 10 , 11 , 12,
+     * //X  , X  , 14 , 13 , 12 ,
      */
     fun getLightSections(max: Int = MAX_SECTIONS_COUNT, spanCount: Int = SPAN_COUNT, count: Int): MutableList<LightSection>? {
         if (max < 0 || spanCount <= 0 || count < MIN_SECTIONS_COUNT || count > MAX_SECTIONS_COUNT) {
-            Log.e("asuralxd", "please check input!")
+            Log.e("getLightSections", "params error, please check input!")
             return null
         }
-        val list: MutableList<LightSection> = mutableListOf()
+        //step1: 计算真实行数及最后一行个数
         val rows = count / spanCount
         val more = count % spanCount
-        //计算真实行数及最后一行个数
         var realRows = 0
         var realMore = 0
         if (rows == 1 && more == 0) {
@@ -66,9 +67,9 @@ object LightSectionsUtils {
                 }
             }
         }
-        Log.d("asuralxd", "------------------------------------")
-        Log.d("asuralxd", "max $max, spanCount:$spanCount, count $count, realRows $realRows, realMore $realMore")
-        //奇数行位置一致，偶数行位置反序
+        Log.d("getLightSections", "max $max, spanCount:$spanCount, count $count, realRows $realRows, realMore $realMore")
+        val list: MutableList<LightSection> = mutableListOf()
+        //step2: 奇数行位置一致，偶数行位置反序,空白位置补全占位空格
         for (row in 0 until realRows) {
             val lastRow = row == (realRows - 1)
             val needTurning = if (row == 0) {
@@ -78,7 +79,6 @@ object LightSectionsUtils {
                 //不是最后一行，都需要换行
                 !lastRow
             }
-
             if ((row + 1) % 2 == 1) {
                 //奇数行
                 val positionStart = if (row == 0) {
@@ -108,7 +108,7 @@ object LightSectionsUtils {
                 val indexStart = positionStart
                 val indexEnd = positionEnd
                 Log.d(
-                    "asuralxd",
+                    "getLightSections",
                     "count $count, 奇数 row $row, index:$indexStart -> $indexEnd ,needTurning $needTurning , position:$positionStart->$positionEnd"
                 )
                 for (position in positionStart..positionEnd) {
@@ -124,7 +124,7 @@ object LightSectionsUtils {
                     val lightSection = LightSection(position, position, shape, false, "$position,$position,$position")
                     list.add(lightSection)
                 }
-                Log.d("asuralxd", "奇数 \n $list")
+                Log.d("getLightSections", "奇数 \n $list")
             } else {
                 //偶数行反序，需要占位空格
                 val positionStart = SPAN_COUNT + (row - 1) * (SPAN_COUNT - 1)
@@ -138,12 +138,9 @@ object LightSectionsUtils {
                 val indexEnd = positionStart
 
                 Log.d(
-                    "asuralxd",
+                    "getLightSections",
                     "count $count, 偶数 row $row, index:$indexStart -> $indexEnd ,needTurning $needTurning , position:$positionStart->$positionEnd"
                 )
-                //                 5  6 7 8
-                //                -1 -1 6 5
-                //                    2
                 for (position in positionStart..positionEnd) {
                     if (lastRow && realMore != 0) {
                         //尾行且未占满的情况，计算空格
@@ -175,43 +172,10 @@ object LightSectionsUtils {
                         list.add(lightSection)
                     }
                 }
-                Log.d("asuralxd", "偶数 \n $list")
+                Log.d("getLightSections", "偶数 \n $list")
             }
         }
-
-
-        Log.d("asuralxd", "getLightSections \n $list")
+        Log.d("getLightSections", "count $count, list: \n $list")
         return list
     }
-
-    private fun exchangeIndex(position: Int, count: Int, max: Int = MAX_SECTIONS_COUNT, spanCount: Int = SPAN_COUNT): Int {
-        val rows = count / spanCount
-        val more = count % spanCount
-        //计算真实行数及最后一行个数
-        var realRows = 0
-        var realMore = 0
-        if (rows == 0 || (rows == 1 && more == 0)) {
-            realRows = 1
-        } else {
-            //首行以外的个数 % 其余行个数
-            realMore = (count - SPAN_COUNT) % (SPAN_COUNT - 1)  //加上转弯重复的1个
-            realRows = if (realMore == 0) {
-                //除第一行放SPAN_COUNT个外，其余都只能放(SPAN_COUNT-1)个
-                (count - SPAN_COUNT) / (SPAN_COUNT - 1) + 1 //加上首行
-            } else {
-                //除第一行放SPAN_COUNT个外，其余都只能放(SPAN_COUNT-1)个
-                (count - SPAN_COUNT) / (SPAN_COUNT - 1) + 1 + 1 //加上首行,有余数需要换行
-            }
-        }
-
-
-
-
-
-
-
-
-        return 0
-    }
-
 }
